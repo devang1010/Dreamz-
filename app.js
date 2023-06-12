5792c3d0bf4e6cf3137d922eeb5baf405debb956
Devang Thaker <devangt101003@gamil.com>
2023-06-12 14:41:18 +0530

    commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 
+
+
 const app=express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,148 +40,148 @@ app.get("/sandwich", function(req, res){
 app.listen(3000);
 
 
-gsap.from(".nav-link", {
-    duration: 1.5,
-    opacity: 0
-});
+// gsap.from(".nav-link", {
+//     duration: 1.5,
+//     opacity: 0
+// });
 
 
-// ------------------------- Hero Section
-gsap.from(".animate-hero", {
-    duration: 1,
-    y: '-100%',
-    ease: 'linear',
-    stagger: 0.3,
-    opacity: 0,
-    delay: 0.6
-});
+// // ------------------------- Hero Section
+// gsap.from(".animate-hero", {
+//     duration: 1,
+//     y: '-100%',
+//     ease: 'linear',
+//     stagger: 0.3,
+//     opacity: 0,
+//     delay: 0.6
+// });
 
-gsap.from(".button", {
-    duration: 1,
-    opacity: 0
-});
+// gsap.from(".button", {
+//     duration: 1,
+//     opacity: 0
+// });
 
-// ------------------------ Service Section
-gsap.from(".animate-service", {
-    scrollTrigger: '.animate-service',
-    duration: 0.5,
-    x: '-30%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.12,
-    opacity: 0
-});
+// // ------------------------ Service Section
+// gsap.from(".animate-service", {
+//     scrollTrigger: '.animate-service',
+//     duration: 0.5,
+//     x: '-30%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.12,
+//     opacity: 0
+// });
 
-gsap.from(".animate-img", {
-    scrollTrigger: '.animate-img',
-    duration: 0.5,
-    x: '30%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.12,
-    opacity: 0
-});
+// gsap.from(".animate-img", {
+//     scrollTrigger: '.animate-img',
+//     duration: 0.5,
+//     x: '30%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.12,
+//     opacity: 0
+// });
 
-// ------------------ Membership Section
-gsap.from(".animate-membership", {
-    scrollTrigger: '.animate-membership',
-    duration: 1.6,
-    y: '-80%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0
-});
+// // ------------------ Membership Section
+// gsap.from(".animate-membership", {
+//     scrollTrigger: '.animate-membership',
+//     duration: 1.6,
+//     y: '-80%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0
+// });
 
-gsap.from(".animate-card", {
-    scrollTrigger: '.animate-card',
-    duration: 0.7,
-    y: '30%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0,
-    delay: 0.2
-});
+// gsap.from(".animate-card", {
+//     scrollTrigger: '.animate-card',
+//     duration: 0.7,
+//     y: '30%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0,
+//     delay: 0.2
+// });
 
-// ------------------------ Team Section
-gsap.from(".animate-team1", {
-    scrollTrigger: '.animate-team1',
-    duration: 0.3,
-    y: '-80%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0,
-    delay: 0.7
-});
+// // ------------------------ Team Section
+// gsap.from(".animate-team1", {
+//     scrollTrigger: '.animate-team1',
+//     duration: 0.3,
+//     y: '-80%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0,
+//     delay: 0.7
+// });
 
-gsap.from(".animate-team2", {
-    scrollTrigger: '.animate-team2',
-    duration: 0.3,
-    y: '-80%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0,
-    delay: 0.7
-});
+// gsap.from(".animate-team2", {
+//     scrollTrigger: '.animate-team2',
+//     duration: 0.3,
+//     y: '-80%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0,
+//     delay: 0.7
+// });
 
-gsap.from(".animate-img1", {
-    scrollTrigger: '.animate-img1',
-    duration: 0.7,
-    x: '-30%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0,
-    delay: 0.5
-});
+// gsap.from(".animate-img1", {
+//     scrollTrigger: '.animate-img1',
+//     duration: 0.7,
+//     x: '-30%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0,
+//     delay: 0.5
+// });
 
-gsap.from(".animate-img2", {
-    scrollTrigger: '.animate-img2',
-    duration: 0.7,
-    x: '30%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0,
-    delay: 0.5
-});
+// gsap.from(".animate-img2", {
+//     scrollTrigger: '.animate-img2',
+//     duration: 0.7,
+//     x: '30%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0,
+//     delay: 0.5
+// });
 
-gsap.from(".animate-img3", {
-    scrollTrigger: '.animate-img3',
-    duration: 0.9,
-    x: '-30%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0
-});
+// gsap.from(".animate-img3", {
+//     scrollTrigger: '.animate-img3',
+//     duration: 0.9,
+//     x: '-30%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0
+// });
 
-gsap.from(".animate-img4", {
-    scrollTrigger: '.animate-img4',
-    duration: 0.9,
-    x: '30%',
-    ease: 'linear',
-    scrub: 3,
-    stagger: 0.3,
-    opacity: 0,
-});
+// gsap.from(".animate-img4", {
+//     scrollTrigger: '.animate-img4',
+//     duration: 0.9,
+//     x: '30%',
+//     ease: 'linear',
+//     scrub: 3,
+//     stagger: 0.3,
+//     opacity: 0,
+// });
 
-// --------------------- Email section
-gsap.from(".animate-email", {
-    scrollTrigger: '.animate-email',
-    duration: 1,
-    opacity: 0,
-    stagger: 0.5
-});
+// // --------------------- Email section
+// gsap.from(".animate-email", {
+//     scrollTrigger: '.animate-email',
+//     duration: 1,
+//     opacity: 0,
+//     stagger: 0.5
+// });
 
-// ----------------------- Footer section
-gsap.from("footer", {
-    scrollTrigger: 'footer',
-    duration: 0.5,
-    opacity: 0,
-    stagger: 0.5,
-    y: '50%'
-});
+// // ----------------------- Footer section
+// gsap.from("footer", {
+//     scrollTrigger: 'footer',
+//     duration: 0.5,
+//     opacity: 0,
+//     stagger: 0.5,
+//     y: '50%'
+// });
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import gsap from "gsap";
+
+// get other plugins:
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Flip from "gsap/Flip";
+import Draggable from "gsap/Draggable";
+
+// or all tools are exported from the "all" file (excluding members-only plugins):
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+
+// don't forget to register plugins
+gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin); 
+
 gsap.from(".nav-link", {
     duration: 1.5,
     opacity: 0
