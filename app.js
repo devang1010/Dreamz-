f9c4164bb9895cf647979d6a3e3d3323598ee7d1
Devang Thaker <devangt101003@gamil.com>
2023-06-14 14:58:16 +0530

    final

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.get("/", function (req, res) {
 
 });
 
+app.get("/home", function(req, res){
+    res.render('index');
+})
+
 app.get("/login", function (req, res) {
     console.log("Running!!");
     res.render('login');
